1dc70bf41c5a54662ada1bfefd89e96b38242e22
xi.chen <xi.chen@useepay.com>
2024-05-22 15:45:28 +0800

    ✨feature-KJSD-2

--- chenxiDemo/src/main/java/leetCode/JumpGame55.java
+++ chenxiDemo/src/main/java/leetCode/JumpGame55.java
@@ -42,9 +42,8 @@ public class JumpGame55 {
     @Test
     public void gitTest() {
         System.out.println("feature-KJSD-2 update");
-        System.out.println("feature-KJSD-1 update");
-        System.out.println("feature-KJSD-1 update1");
-        System.out.println("回退到这个版本");
+        System.out.println("feature-KJSD-2 update 1");
+        System.out.println("feature-KJSD-2 update 2");
     }
 
 
