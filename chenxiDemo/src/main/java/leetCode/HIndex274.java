f14968e4a129f6ab144bf3139da13ee19028bd7c
xi.chen <xi.chen@useepay.com>
2024-07-25 15:58:51 +0800

    333

--- chenxiDemo/src/main/java/leetCode/HIndex274.java
+++ chenxiDemo/src/main/java/leetCode/HIndex274.java
@@ -42,5 +42,7 @@ public class HIndex274 {
 //        int[] intArr = new int[]{1, 3, 1};
         System.out.println(hIndex(intArr));
         System.out.println(111);
+        System.out.println(222);
+        System.out.println(333);
     }
 }
